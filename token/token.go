0f5e67b4c00d725d56da2f1ce3516b084fed5d90
Mustafa Muhammad <muhammadmustafa4000@gmail.com>
2025-06-15 18:26:36 -0400

    Add more tests

--- token/token.go
+++ token/token.go
@@ -29,11 +29,21 @@ const (
 
   FUNCTION   = "FUNCTION"
   LET        = "LET"
+  TRUE       = "TRUE"
+  FALSE      = "FALSE"
+  IF         = "IF"
+  ELSE       = "ELSE"
+  RETURN     = "RETURN"
 )
 
 var keywords = map[string]TokenType {
-  "fn" : FUNCTION,
-  "let": LET,
+  "fn"     : FUNCTION,
+  "let"    : LET,
+  "false"  : FALSE,
+  "true"   : TRUE,
+  "if"     : IF,
+  "else"   : ELSE,
+  "return" : RETURN,
 }
 
 func LookupIdent(identString string) TokenType{
