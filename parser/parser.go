9c385836cae7fdd8f9669cfa786d286d696b7583
Mustafa Muhammad <muhammadmustafa4000@gmail.com>
2025-06-15 22:59:50 -0400

    Save

--- parser/parser.go
+++ parser/parser.go
@@ -7,6 +7,11 @@ import (
 	"fmt"
 )
 
+type (
+  prefixParseFn func() ast.Expression
+  infixParseFn  func(ast.Expression) ast.Expression
+)
+
 type Parser struct {
   l         *lexer.Lexer
   curToken  token.Token
